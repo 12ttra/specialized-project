7d422081dd00a7ed28ade9d172c90015185195c0
NhuNhy <82555469+xrnhy2911@users.noreply.github.com>
2023-03-31 20:34:30 +0700

    Done page cartnew+old

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 
 function route(app){
+    
     app.use('/account/login',(req, res)=>{
         res.render('pages/login', { title: 'Login', layout: 'no-header-footer' });
     })
+      
+    app.use('/account/register',(req, res)=>{
+        res.render('pages/register', { title: 'Register', layout: 'no-header-footer' });
+    })
+
+    app.use('/category/catalog-cart',(req, res)=>{
+        res.render('pages/catelog-cart');
+    })
+
+    
+    
     app.get('/', (req, res) => {
         res.render('home');
     })
