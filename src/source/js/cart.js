e869c05cfa8e71c429f0087c71f5c62bcb7da3e5
12ttra <hsgilcongtu@gmail.com>
2023-04-09 19:12:10 +0700

    update

--- src/source/js/cart.js
+++
@@ -1,8 +0,0 @@
-import "./cart.css";
-import { Link } from "react-router-dom";
-
-const CartItem = ({ item, qtyChangeHandler, removeHandler }) => {
-  return ('./cart.hbs');
-};
-
-export default CartItem;
