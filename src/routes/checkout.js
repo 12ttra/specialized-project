ba587208cc5f9a8be5686064894bddddd24bc31a
NhuNhy <82555469+xrnhy2911@users.noreply.github.com>
2023-04-13 13:40:14 +0700

    done page checkout

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const route = require('.');
 const router = express.Router();
+
 
 router.use('/cart',(req, res)=>{
     res.render('pages/cart');
 })
 
-app.use('/address',(req, res)=>{
+router.use('/address',(req, res)=>{
     res.render('pages/checkout');
 })
 
-app.use('/payment',(req, res)=>{
+router.use('/payment',(req, res)=>{
     res.render('pages/payment');
 })
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+const checkoutRouter = require('./checkout');
 const catalogRouter = require('./product');
 const categoryRouter = require('./category');
 const accountRouter = require('./account');
@@ -6,6 +7,7 @@ function route(app){
     //product
     app.use("/product",catalogRouter);
     app.use("/category",categoryRouter);
+    app.use("/checkout",checkoutRouter);
     app.use("/account",accountRouter);
     app.use('/admin/dashboard',(req, res)=>{
         res.render('admin/dashboard', { title: 'Dashboard', layout: 'admin' });
