e3f86e6abf969ea04783c64421d5de55596dbb5e
Trà Trần <hsgilcongtu@gmail.com>
2023-04-28 17:49:10 +0700

    #5

--- src/config/db/mongo.js
+++ src/config/db/mongo.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 async function connectDB(){
     try {
         await mongoose.connect('mongodb+srv://tra:123@cluster0.rb2ac3u.mongodb.net/Sprezza?retryWrites=true&w=majority');
-        console.log("connect successfuly");
+        console.log("DB connect successfuly");
     } catch (error) {
         console.log(error);
     }
