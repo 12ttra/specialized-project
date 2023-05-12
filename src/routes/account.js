5061dfff1ff6bc15dda895fd4e2d9773391eb13f
Thanh Nhi <19521964@gm.uit.edu.vn>
2023-05-13 04:23:16 +0700

    update controller

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require("../controller/AccountController");
-const accountControlers = require("../app/controllers/AccountController");
 
 router.post('/login', accountController.logIn)
 router.post('/register',accountController.signUp)
-router.get('/information',accountControlers.information)
-router.get('/register-store', accountControlers.store)
-
-
-
+router.get('/information',accountController.information)
+router.get('/register-store', accountController.store)
 
 module.exports = router;
