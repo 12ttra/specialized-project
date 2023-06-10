26b9e3e82eaf07abddc033d2258803fa27bf50c9
Trà Trần <hsgilcongtu@gmail.com>
2023-06-10 13:18:27 +0700

    creat shop

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const shopController = require("../controller/ShopController");
 
 //signUp
-//router.post('/myshop/signup',shopController.signUp)
+router.post('/myshop/signup',shopController.signUp)
 router.post('/myshop',shopController.index)
 
 module.exports = router;
