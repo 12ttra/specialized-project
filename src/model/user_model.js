78bd735c73878de589ab25ce369a1bbd7d66973a
Trà Trần <hsgilcongtu@gmail.com>
2023-06-15 21:00:35 +0700

    update

--- src/model/user_model.js
+++ src/model/user_model.js
@@ -3,31 +3,47 @@ const bcrypt = require("bcrypt-nodejs");
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  username: {
+  name: {
     type: String,
-    require: true,
-    unique: true
+    required: true,
+    maxlength: 32,
   },
   email: {
     type: String,
-    require: true,
-    unique: true
+    required: true,
+    trim: true,
+    index: { unique: true },
+    match: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
   },
   password: {
     type: String,
-    require: true,
+    required: true,
   },
-  mobile:{
-    type:String,
-    required:true,
-    unique:true,
+  userRole: {
+    type: Number,
+    required: true,
   },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  }
+  phoneNumber: {
+    type: Number,
+  },
+  userImage: {
+    type: String,
+    default: "user.png",
+  },
+  verified: {
+    type: String,
+    default: false,
+  },
+  secretKey: {
+    type: String,
+    default: null,
+  },
+  history: {
+    type: Array,
+    default: [],
+  },
 },
-{ timestamps: true}
+{ timestamps: true }
 );
 
 // encrypt the password before storing
