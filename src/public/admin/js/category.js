016b2c211a59cb2a5b79c38e5565373926ea3f93
NhuNhy <82555469+xrnhy2911@users.noreply.github.com>
2023-06-17 16:12:59 +0700

    trang addnew catregory.js

---
+++ src/public/admin/js/category.js
@@ -0,0 +1,8 @@
+const btnSave = document.getElementById("#add-category #save-category");
+if(btnSave){
+    btnSave.addEventListener("click", addNewCategory);
+}
+
+function addNewCategory(){
+   
+}
