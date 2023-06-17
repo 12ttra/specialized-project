61d72d883c2b90b0e3f5cd5071dc4544de83df2c
Trà Trần <hsgilcongtu@gmail.com>
2023-06-17 12:16:18 +0700

    Update accountController

--- src/controller/AccountController.js
+++ src/controller/AccountController.js
@@ -34,11 +34,11 @@ class AccountController{
         password: "Filed must not be empty",
         cPassword: "Filed must not be empty",
       };
-      return res.json({ error });
+      return res.status(500).json({ error });
     }
     if (name.length < 3 || name.length > 25) {
       error = { ...error, name: "Name must be 3-25 charecter" };
-      return res.json({ error });
+      return res.status(500).json({ error });
     } else {
       if (validateEmail(email)) {
         name = toTitleCase(name);
@@ -49,7 +49,7 @@ class AccountController{
             name: "",
             email: "",
           };
-          return res.json({ error });
+          return res.status(500).json({ error });
         } else {
           // If Email & Number exists in Database then:
           try {
@@ -62,7 +62,7 @@ class AccountController{
                 name: "",
                 email: "Email already exists",
               };
-              return res.json({ error });
+              return res.status(500).json({ error });
             } else {
               let newUser = new userModel({
                 name,
@@ -74,7 +74,7 @@ class AccountController{
               newUser
                 .save()
                 .then((data) => {
-                  return res.json({
+                  return res.status(200).json({
                     success: "Account create successfully. Please login",
                   });
                 })
@@ -93,7 +93,7 @@ class AccountController{
           name: "",
           email: "Email is not valid",
         };
-        return res.json({ error });
+        return res.status(500).json({ error });
       }
     }
   }
@@ -102,14 +102,14 @@ class AccountController{
   async postSignin(req, res) {
     let { email, password } = req.body;
     if (!email || !password) {
-      return res.json({
+      return res.status(500).json({
         error: "Fields must not be empty",
       });
     }
     try {
       const data = await userModel.findOne({ email: email });
       if (!data) {
-        return res.json({
+        return res.status(500).json({
           error: "Invalid email or password",
         });
       } else {
@@ -120,18 +120,48 @@ class AccountController{
             JWT_SECRET
           );
           const encode = jwt.verify(token, JWT_SECRET);
-          return res.json({
+          return res.status(200).json({
             token: token,
             user: encode,
           });
         } else {
-          return res.json({
+          return res.status(500).json({
             error: "Invalid email or password",
           });
         }
       }
     } catch (err) {
       console.log(err);
+    }
+  }
+
+  async changePassword(req, res) {
+    let { uId, oldPassword, newPassword } = req.body;
+    if (!uId || !oldPassword || !newPassword) {
+      return res.json({ message: "All filled must be required" });
+    } else {
+      const data = await userModel.findOne({ _id: uId });
+      if (!data) {
+        return res.status(500).json({
+          error: "Invalid user",
+        });
+      } else {
+        const oldPassCheck = await bcrypt.compare(oldPassword, data.password);
+        if (oldPassCheck) {
+          newPassword = bcrypt.hashSync(newPassword, 10);
+          let passChange = userModel.findByIdAndUpdate(uId, {
+            password: newPassword,
+          });
+          passChange.exec((err, result) => {
+            if (err) console.log(err);
+            return res.status(200).json({ success: "Password updated successfully" });
+          });
+        } else {
+          return res.status(500).json({
+            error: "Your old password is wrong!!",
+          });
+        }
+      }
     }
   }
 }
